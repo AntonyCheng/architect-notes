02dab49cbae3a6aae04b293ac7681c36a5f8f337
AntonyCheng <1911261716@qq.com>
2024-12-02 09:48:05 +0800

    architect-notes 0.1.3 JVM字节码文件结构深度剖析（了解）

---
+++ 1.性能优化/12.JVM字节码文件结构深度剖析（了解）/src/main/java/com/tuling/smlz/jvm/classbyatecode/TulingByteCode.java
@@ -0,0 +1,20 @@
+package com.tuling.smlz.jvm.classbyatecode;
+
+/**
+ * 请结合TulingByteCode.json和字节码分析参考资料解读该类的class字节码二进制文件
+ *
+ * @author AntonyCheng
+ */
+public class TulingByteCode {
+
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+}
